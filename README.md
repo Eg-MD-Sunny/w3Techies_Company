32a877c5df86b448959dd5573910edb7b6eca486
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-06-24 12:55:57 +0600

    Complate Web Design Project

---
+++ README.md
@@ -0,0 +1,1 @@
+# w3Techies_Company
---
+++ js/custom.js
@@ -0,0 +1,34 @@
+//(function ($) {
+//    "use strict";
+
+//// Owl-Carosul Active===============>>>	
+	// $(document).ready(function () {
+       
+	// 	$('.custom1').owlCarousel({
+	// 		items: 1,
+	// 		loop: true,
+	// 		animateOut: 'slideOutDown',
+ //    		animateIn: 'bounceInUp',
+	// 		autoplay:true,
+	// 		nav:true,
+	// 		navText:['<i class="fa fa-angle-up"</i>','<i class="fa fa-angle-down"</i>'],
+	// 	});
+		
+// Owl-Carosul Event Animation Active================================>>>	
+//		
+	// $('.custom1').on('translate.owl.carousel', function (){
+	// 	$('.item h1').removeClass('fadeInDown animated');
+	// 	$('.item p').removeClass('fadeInLeft animated');
+	// 	$('.item .btn_eff').removeClass('fadeInUp animated');
+	// });
+	
+	// $('.custom1').on('translated.owl.carousel', function(){
+	// 	$('.owl-item.active .item h1').addClass('fadeInDown animated');			
+	// 	$('.owl-item.active .item p').addClass('fadeInLeft animated');			
+	// 	$('.owl-item.active .item .btn_eff').addClass('fadeInUp animated');			
+			
+	// });	
+//	
+//	
+
+//})(jQuery);
